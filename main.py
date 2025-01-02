e8ef1b6ecc1fb8ada039a86addcdf82756f27c8d
Shruti9826 <111629897+Shruti9826@users.noreply.github.com>
2025-01-03 00:01:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi, {}'.format(name)  # Compatible with older Python versions
+    print('Hi, {}'.format(name))  # Compatible with older Python versions
     print("Hi please be calm everything will be fine soon") # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == '__main__'
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
